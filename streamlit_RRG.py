d9ffc6a1624e8c41f7b76200be4b7fcf46d0ddda
jasonckb <122160477+jasonckb@users.noreply.github.com>
2024-08-28 20:13:44 +0800

    Update streamlit_RRG.py

--- streamlit_RRG.py
+++ streamlit_RRG.py
@@ -115,7 +115,7 @@ def get_data(universe, sector, timeframe, custom_tickers=None, custom_benchmark=
         sector_names = {
             "GBPUSD=X": "GBP", "EURUSD=X": "EUR", "AUDUSD=X": "AUD", "NZDUSD=X": "NZD",
             "CADUSD=X": "CAD",  "JPYUSD=X": "JPY", "EURGBP=X": "EURGBP", "AUDNZD=X": "AUDNZD",
-            "AUDCAD=X": "AUDCAD", "NZDCAD=X": "NZDCAD", "DX-Y.NYB":"DXY", "CHFUSD=X":"CHF","CNHUSD=X":"CNH" 
+            "AUDCAD=X": "AUDCAD", "NZDCAD=X": "NZDCAD", "DX-Y.NYB":"DXY", "CNHUSD=X":"CHF","CNHUSD=X":"CNH" 
         }
     else:
         st.error("Invalid universe selection.")
